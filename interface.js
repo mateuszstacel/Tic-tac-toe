c4bdffdbf371fc5b37425bcc7795dac0a5f4c460
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-17 17:25:06 +0000

    Method pushing user choice to array

--- interface.js
+++ interface.js
@@ -4,38 +4,47 @@ $(document).ready(function() {
 
 $('#square1').click(function() {
   tictac.assignToSquare(1)
+  tictac.choice(0, 0)
 });
 
 $('#square2').click(function() {
   tictac.assignToSquare(2)
+  tictac.choice(0, 1)
 });
 
 $('#square3').click(function() {
   tictac.assignToSquare(3)
+  tictac.choice(0, 2)
 });
 
 $('#square4').click(function() {
   tictac.assignToSquare(4)
+  tictac.choice(1, 0)
 });
 
 $('#square5').click(function() {
   tictac.assignToSquare(5)
+  tictac.choice(1, 1)
 });
 
 $('#square6').click(function() {
   tictac.assignToSquare(6)
+  tictac.choice(1, 2)
 });
 
 $('#square7').click(function() {
   tictac.assignToSquare(7)
+  tictac.choice(2, 0)
 });
 
 $('#square8').click(function() {
   tictac.assignToSquare(8)
+  tictac.choice(2, 1)
 });
 
 $('#square9').click(function() {
   tictac.assignToSquare(9)
+  tictac.choice(2, 2)
 });
 
 });
--- src/game.js
+++ src/game.js
@@ -9,17 +9,16 @@ var square8;
 var square9;
 var player_1_move = true
 var player_2_move = false
+var array = [[],[],[]];
 
 function Tictac() {
 };
 
-Tictac.prototype.choice = function(squareNumber) {
+Tictac.prototype.choice = function(index1, index2) {
   if (player_1_move === true) {
-    moves.push("x")
-    player_1_move = false
-    player_2_move = true
+    array[index1][index2] = "x"
   } else if (player_2_move === true){
-    moves.push("o")
+    array[index1][index2] = "o"
     player_2_move = false
     player_2_move = true
   };
@@ -28,10 +27,12 @@ Tictac.prototype.choice = function(squareNumber) {
 Tictac.prototype.assignToSquare = function(number){
   if(player_1_move === true) {
     $('#div'+number+'').text("x");
+    $('#div'+number+'').css("color", "red");
     player_1_move = false
     player_2_move = true
   }else if(player_2_move === true) {
     $('#div'+number+'').text("o");
+    $('#div'+number+'').css("color", "blue");
     player_1_move = true
     player_2_move = false
   };

6aa5ca4027a5c1a24962aae1e1f52aa292216914
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-19 11:40:07 +0000

    Added check score, JQuery, locking button after click

--- interface.js
+++ interface.js
@@ -3,48 +3,66 @@ var tictac = new Tictac();
 $(document).ready(function() {
 
 $('#square1').click(function() {
+  tictac.score()
   tictac.assignToSquare(1)
   tictac.choice(0, 0)
+  tictac.stopEachBtn(1)
 });
 
 $('#square2').click(function() {
+  tictac.score()
   tictac.assignToSquare(2)
   tictac.choice(0, 1)
+  tictac.stopEachBtn(2)
 });
 
 $('#square3').click(function() {
+  tictac.score()
   tictac.assignToSquare(3)
   tictac.choice(0, 2)
+  tictac.stopEachBtn(3)
 });
 
 $('#square4').click(function() {
+  tictac.score()
   tictac.assignToSquare(4)
   tictac.choice(1, 0)
+  tictac.stopEachBtn(4)
 });
 
 $('#square5').click(function() {
+  tictac.score()
   tictac.assignToSquare(5)
   tictac.choice(1, 1)
+  tictac.stopEachBtn(5)
 });
 
 $('#square6').click(function() {
+  tictac.score()
   tictac.assignToSquare(6)
   tictac.choice(1, 2)
+  tictac.stopEachBtn(6)
 });
 
 $('#square7').click(function() {
+  tictac.score()
   tictac.assignToSquare(7)
   tictac.choice(2, 0)
+  tictac.stopEachBtn(7)
 });
 
 $('#square8').click(function() {
+  tictac.score()
   tictac.assignToSquare(8)
   tictac.choice(2, 1)
+  tictac.stopEachBtn(8)
 });
 
 $('#square9').click(function() {
+  tictac.score()
   tictac.assignToSquare(9)
   tictac.choice(2, 2)
+  tictac.stopEachBtn(9)
 });
 
 });
--- src/game.js
+++ src/game.js
@@ -1,17 +1,33 @@
 var player_1_move = true
 var player_2_move = false
-var array = [[],[],[]];
+var grid = [[null, null, null], [null, null, null], [null, null, null]];
+var check = 0
+var player_name = ''
 
 function Tictac() {
 };
 
+Tictac.prototype.score = function() {
+  for(i = 0; i <= 2; i++){
+    if (grid[i][0] != null && grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]){
+      $('#container').text('Player with ' +player_name+ ' won');
+    }if (grid[0][i] != null && grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i]){
+      $('#container').text('Player with ' +player_name+ ' won');
+   }if (grid[0][0] != null && grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]){
+     $('#container').text('Player with ' +player_name+ ' won');
+   }if (grid[0][2] != null && grid[0][2] === grid[1][1] && grid[0][2] === grid[2][0]){
+     $('#container').text('Player with ' +player_name+ ' won');
+   };
+ };
+};
+
 Tictac.prototype.choice = function(index1, index2) {
   if (player_1_move === true) {
-    array[index1][index2] = "x"
-  } else if (player_2_move === true){
-    array[index1][index2] = "o"
-    player_2_move = false
-    player_2_move = true
+    player_name = "o"
+    grid[index1][index2] = "o"
+  }else if (player_2_move === true){
+    player_name = "x"
+    grid[index1][index2] = "x"
   };
 };
 
@@ -25,22 +41,35 @@ Tictac.prototype.assignToSquare = function(number){
   };
 };
 
-Tictac.prototype.playerTwoSwitchTurn = function() {
-  player_1_move = true
-  player_2_move = false
+
+Tictac.prototype.Draw = function() {
+  if (check === 9){
+    $('#container').text("Draw");
+  }else {
+  check +=1
+  };
 };
 
-Tictac.prototype.playerOneSwitchTurn = function() {
-  player_1_move = false
-  player_2_move = true
-};
+  Tictac.prototype.playerTwoSwitchTurn = function() {
+    player_1_move = true
+    player_2_move = false
+  };
 
-Tictac.prototype.playerOneChoice = function(number) {
-  $('#div'+number+'').text("x");
-  $('#div'+number+'').css("color", "red");
-};
+  Tictac.prototype.playerOneSwitchTurn = function() {
+    player_1_move = false
+    player_2_move = true
+  };
 
-Tictac.prototype.playerTwoChoice = function(number) {
-  $('#div'+number+'').text("o");
-  $('#div'+number+'').css("color", "blue");
-};
+  Tictac.prototype.playerOneChoice = function(number) {
+    $('#div'+number+'').text("x");
+    $('#div'+number+'').css("color", "red");
+  };
+
+  Tictac.prototype.playerTwoChoice = function(number) {
+    $('#div'+number+'').text("o");
+    $('#div'+number+'').css("color", "blue");
+  };
+
+  Tictac.prototype.submitBtn= function(number) {
+    $('#square'+number+'').attr("disabled", true);
+  };

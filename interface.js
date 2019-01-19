67cf78257b6f6ad47c9daff2666b5610476ac8e0
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-19 12:36:44 +0000

    Buttons after click are not avaible, JQuery, Css

--- interface.js
+++ interface.js
@@ -1,68 +1,83 @@
 var tictac = new Tictac();
 
 $(document).ready(function() {
+  $('#new-game').hide()
 
 $('#square1').click(function() {
-  tictac.score()
   tictac.assignToSquare(1)
   tictac.choice(0, 0)
   tictac.stopEachBtn(1)
+  tictac.draw()
+  tictac.score()
+
 });
 
 $('#square2').click(function() {
-  tictac.score()
   tictac.assignToSquare(2)
   tictac.choice(0, 1)
   tictac.stopEachBtn(2)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square3').click(function() {
-  tictac.score()
   tictac.assignToSquare(3)
   tictac.choice(0, 2)
   tictac.stopEachBtn(3)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square4').click(function() {
-  tictac.score()
   tictac.assignToSquare(4)
   tictac.choice(1, 0)
   tictac.stopEachBtn(4)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square5').click(function() {
-  tictac.score()
   tictac.assignToSquare(5)
   tictac.choice(1, 1)
   tictac.stopEachBtn(5)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square6').click(function() {
-  tictac.score()
   tictac.assignToSquare(6)
   tictac.choice(1, 2)
   tictac.stopEachBtn(6)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square7').click(function() {
-  tictac.score()
   tictac.assignToSquare(7)
   tictac.choice(2, 0)
   tictac.stopEachBtn(7)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square8').click(function() {
-  tictac.score()
   tictac.assignToSquare(8)
   tictac.choice(2, 1)
   tictac.stopEachBtn(8)
+  tictac.draw()
+  tictac.score()
 });
 
 $('#square9').click(function() {
-  tictac.score()
   tictac.assignToSquare(9)
   tictac.choice(2, 2)
   tictac.stopEachBtn(9)
+  tictac.draw()
+  tictac.score()
+});
+
+$('#new-game').click(function() {
+  location.reload()
 });
 
 });
--- src/game.js
+++ src/game.js
@@ -10,13 +10,13 @@ function Tictac() {
 Tictac.prototype.score = function() {
   for(i = 0; i <= 2; i++){
     if (grid[i][0] != null && grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]){
-      $('#container').text('Player with ' +player_name+ ' won');
+      this.printWinner();
     }if (grid[0][i] != null && grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i]){
-      $('#container').text('Player with ' +player_name+ ' won');
+      this.printWinner();
    }if (grid[0][0] != null && grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]){
-     $('#container').text('Player with ' +player_name+ ' won');
+     this.printWinner();
    }if (grid[0][2] != null && grid[0][2] === grid[1][1] && grid[0][2] === grid[2][0]){
-     $('#container').text('Player with ' +player_name+ ' won');
+     this.printWinner();
    };
  };
 };
@@ -42,9 +42,10 @@ Tictac.prototype.assignToSquare = function(number){
 };
 
 
-Tictac.prototype.Draw = function() {
-  if (check === 9){
-    $('#container').text("Draw");
+Tictac.prototype.draw = function() {
+  if (check === 8){
+    $('#result').text("Draw");
+    $('#new-game').show()
   }else {
   check +=1
   };
@@ -70,6 +71,17 @@ Tictac.prototype.Draw = function() {
     $('#div'+number+'').css("color", "blue");
   };
 
-  Tictac.prototype.submitBtn= function(number) {
+  Tictac.prototype.stopEachBtn= function(number) {
     $('#square'+number+'').attr("disabled", true);
+  };
+
+  Tictac.prototype.printWinner = function() {
+    $('#result').text('Player with ' +player_name+ ' won');
+    $('#new-game').show()
+    this.disableAllBtn()
+  };
+
+  Tictac.prototype.disableAllBtn= function() {
+    for (i = 1; i <= 9; i++)
+    $('#square'+i+'').attr("disabled", true);
   };
